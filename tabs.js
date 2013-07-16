68d3bedceed1c90dc8a0246c3f8d763e70359f0b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-16 10:43:19 -0500

    hide / show tabs. Identify id of tab clicked. Show tab content based on id of tab clicked.

--- tabs.js
+++ tabs.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
+  //default to show #tab1
+  $('#tab1').show();
+  // identify click
   $('a').on('click', function(){
+    // hide all tabs
     $('li').removeClass('active');
-    $('this').find('li').addClass('active');
-  })
+    // 'show' tab by adding back class actice of tab that was clicked
+    var tabClicked = $(this).attr('href');
+    $(this).closest('li').addClass('active');
+    // hide text with id=tag#
+    $('.tab').hide();
+    // show tab with the div.id = a.href of the clicked link
+    $(tabClicked).show();
+
+  });
   // capture an event on the screen with jQuery, and then...
   //change the appearace of the page based on that click. 
 });
